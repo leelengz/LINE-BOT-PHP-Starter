6e9b8d18b65dd6f13488f19e1323711c2794b281
Kornthip Nakchamratsri <kornthip@23perspective.com>
2017-10-03 12:14:25 +0700

    menu

--- bot.php
+++ bot.php
@@ -49,21 +49,9 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			$actions = [
-				{
-					'type' => 'postback',
-					'label' => 'Buy',
-					'data' => 'action=buy&itemid=123'
-				},
-				{
-					'type' => 'postback',
-					'label' => 'Add to cart',
-					'data' => 'action=add&itemid=123'
-				},
-				{
-					'type' => 'uri',
-					'label' => 'View detail',
-					'uri' => "http://example.com/page/123"
-				}
+				'type' => 'postback',
+				'label' => 'Buy',
+				'data' => 'action=buy&itemid=123'
 			];
 
 			$columns = [
@@ -104,7 +92,6 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
-
 
 	}
 

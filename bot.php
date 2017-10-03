ed0bfb8a4929b0692034a8ac19dcb32586edc20e
leelengz <kornthip@23perspective.com>
2017-10-03 11:40:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -49,23 +49,7 @@ elseif ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
 	$text = $event['message']['text'];
 	// Get replyToken
 	$replyToken = $event['replyToken'];
-	$actions = [
-		{
-			'type' => 'postback',
-			'label' => 'Buy',
-			'data' => 'action=buy&itemid=123'
-		},
-		{
-			'type' => 'postback',
-			'label' => 'Add to cart',
-			'data' => 'action=add&itemid=123'
-		},
-		{
-			'type' => 'uri',
-			'label' => 'View detail',
-			'uri' => "http://example.com/page/123"
-		}
-	]
+	
 	// Build message to reply back
 	$messages = [
 		'type' => 'location',

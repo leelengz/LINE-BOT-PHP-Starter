6625cc605b224fb5e8cf6fd1e1e29144e0365d65
leelengz <kornthip@23perspective.com>
2017-10-03 11:19:44 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -45,58 +45,6 @@ if (!is_null($events['events'])) {
 	}
 }
 elseif ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'template',
-				'altText' => 'this is a buttons template',
-				'template' => {
-		      'type' => 'buttons',
-		      'thumbnailImageUrl' => "https://example.com/bot/images/image.jpg",
-		      'title' => 'Menu',
-		      'text' => 'Please select',
-		      'actions' => [
-		          {
-		            'type' => 'postback',
-		            'label' => 'Buy',
-		            'data' => 'action=buy&itemid=123'
-		          },
-		          {
-		            'type' => 'postback',
-		            'label' => 'Add to cart',
-		            'data' => 'action=add&itemid=123'
-		          },
-		          {
-		            'type' => 'uri',
-		            'label' => 'View detail',
-		            'uri' => "http://example.com/page/123"
-		          }
-		      ]
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
+			
 	}
 echo "OK";

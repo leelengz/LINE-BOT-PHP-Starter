ca983ef4094eb762b67fc38ae5decf9716b74589
Kornthip Nakchamratsri <kornthip@23perspective.com>
2017-10-03 16:29:54 +0700

    twice

--- bot.php
+++ bot.php
@@ -109,6 +109,9 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
+
+			$result = curl_exec($ch);
+			
 			curl_close($ch);
 
 			echo $result . "\r\n";

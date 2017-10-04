739fd610bfe7d63b9963800c3eb9d7833a3a7824
Kornthip Nakchamratsri <kornthip@23perspective.com>
2017-10-04 14:02:38 +0700

    product

--- product.php
+++ product.php
@@ -118,7 +118,7 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 		}
 
-		elseif ($event['type'] == 'message' && $event['message']['type'] == 'text' && $keyword != 'product') {
+		elseif ($event['type'] == 'message' && $event['message']['type'] == 'text' && $keyword == 'product') {
 			// Get text sent
 			$text = $event['source']['userId'];
 			// Get replyToken
